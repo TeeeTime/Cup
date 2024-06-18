d81d36f6e2e76660df24bea1cf3ce3b58d538f22
Tom <eddie@sinistra.de>
2024-06-18 22:26:10 +0200

    Removed unnecessary Code in Main

--- src/cup/main/Main.java
+++ src/cup/main/Main.java
@@ -13,20 +13,8 @@ public class Main {
 		
 		LiteSQL.connect();
 		SQLManager.onCreate();
-		/*
-		StockManager stockManager = new StockManager();
-		stockManager.addStock("BEE", 500, ":bee:");
-		stockManager.addStock("FUN", 300, ":roller_coaster:");
-		stockManager.addStock("WLD", 600, ":map:");
-		stockManager.addStock("TUK", 200, ":auto_rickshaw:");
-		stockManager.addStock("BLD", 300, ":construction_site:");
-		stockManager.addStock("CRN", 400, ":corn:");
-		stockManager.addStock("EGG", 300, ":egg:");
-		stockManager.addStock("TEA", 1000, ":tea:");
-		stockManager.addStock("BNK", 300, ":bank:");
-		stockManager.addStock("STR", 300, ":sparkles:");
-		stockManager.startScheduler(180);
-		*/
+		
+		
 		DiscordBot bot = new DiscordBot(config.getDiscordAPIToken(), config.getCommandPrefix(), config.getAdminId());
 		bot.getJDA().getPresence().setActivity(Activity.watching("teeestream"));
 	}
