d4c85b6c614037c92a2e0f5a11defedd9c42c239
Tom <eddie@sinistra.de>
2024-06-18 22:39:07 +0200

    Updated WalletCommand
    Updated WalletCommand to support seeing other peoples balance by mentioning them after the initial command trigger

--- src/cup/discordbot/commands/WalletCommand.java
+++ src/cup/discordbot/commands/WalletCommand.java
@@ -10,20 +10,33 @@ import cup.discordbot.DiscordBot;
 import cup.discordbot.ErrorEmbedBuilder;
 import cup.economy.CoinManager;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class WalletCommand implements Command{
 
 	@Override
 	public void execute(MessageReceivedEvent event) {
-		String[] args = event.getMessage().getContentDisplay().substring(1).trim().split(" ");
+		String[] args = event.getMessage().getContentRaw().substring(1).trim().split(" ");
 		
-		if(args.length != 1) {
+		if(args.length > 2) {
 			event.getChannel().sendMessageEmbeds(ErrorEmbedBuilder.usageEmbed(this).build()).queue();
 			return;
 		}
+		User user;
+		if(args.length == 1) {
+			 user = event.getAuthor();
+			
+		}else {
+			if(event.getMessage().getMentions().getMembers().size() != 1) {
+				event.getChannel().sendMessageEmbeds(ErrorEmbedBuilder.usageEmbed(this).build()).queue();
+				return;
+			}
+			
+			user = event.getMessage().getMentions().getMembers().get(0).getUser();
+		}
 		
-		ResultSet results = LiteSQL.onQuery("SELECT count(DISTINCT balance) AS place FROM coins WHERE (balance > " + CoinManager.getCoins(event.getAuthor()) + ")");
+		ResultSet results = LiteSQL.onQuery("SELECT count(DISTINCT balance) AS place FROM coins WHERE (balance > " + CoinManager.getCoins(user) + ")");
 		
 		int place = 1;
 		
@@ -38,17 +51,18 @@ public class WalletCommand implements Command{
 		
 		EmbedBuilder eb = new EmbedBuilder();
 		eb.setColor(Color.ORANGE);
-		eb.setTitle(event.getAuthor().getName() + "'s wallet :moneybag:");
-		eb.setThumbnail(event.getAuthor().getAvatarUrl());
-		eb.addField("Balance:", CoinManager.getCoins(event.getAuthor()) + " :coin:", true);
+		eb.setTitle(user.getName() + "'s wallet :moneybag:");
+		eb.setThumbnail(user.getAvatarUrl());
+		eb.addField("Balance:", CoinManager.getCoins(user) + " :coin:", true);
 		eb.addField("Place:", place + "", true);
 		
 		event.getChannel().sendMessageEmbeds(eb.build()).queue();
+		
 	}
 
 	@Override
 	public String getUsage() {
-		return DiscordBot.INSTANCE.getPrefix() + "wallet";
+		return DiscordBot.INSTANCE.getPrefix() + "wallet <user>";
 	}
 
 	@Override
