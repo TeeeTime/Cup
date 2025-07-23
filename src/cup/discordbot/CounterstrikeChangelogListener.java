b3ff3686206f1501b8200529a85b535968f8045a
Tom <eddie@sinistra.de>
2025-07-23 23:08:13 +0200

    Adjusted Counterstrike update functionality
    Now the Bot will not send the changelog formated, as the method used is way to open for errors making the changelog a big mess. The bot will now just provide the link to the blog post and the already existing comment via chatgpt.

--- src/cup/discordbot/CounterstrikeChangelogListener.java
+++ src/cup/discordbot/CounterstrikeChangelogListener.java
@@ -19,11 +19,9 @@ public class CounterstrikeChangelogListener extends ListenerAdapter{
 			String changelog = blog.extractChangelog(link);
 			TextChannel textChannel = event.getGuild().getTextChannelsByName("counter-strike",true).get(0);
 			
-			textChannel.sendMessage("New update :tada: :tada: :tada:\n\n" + changelog).setSuppressEmbeds(true).queue();
-			
 			ChatGPT chatGPT = new ChatGPT(DiscordBot.INSTANCE.getChatGPTToken());
 			
-			textChannel.sendMessage("\n" + chatGPT.getResponse("You are part of a friend group that plays CS2. You don't use hashtags. You are very casual and very funny and sarcastic. You sometimes use emojis with discord formating, if they amplify your message. Do not disclose that you are an ai, pretend to be just another person on a small discord server. Write a discord message. Make a funny and very sarcastic comment on multiple aspects of this new changelog: " + changelog)).queue();
+			textChannel.sendMessage(link + "\n" + chatGPT.getResponse("You are part of a friend group that plays CS2. You don't use hashtags. You are very casual and very funny and sarcastic. You sometimes use emojis with discord formating, if they amplify your message. Do not disclose that you are an ai, pretend to be just another person on a small discord server. Write a discord message. Make a funny and very sarcastic comment on multiple aspects of this new changelog: " + changelog)).setSuppressEmbeds(true).queue();
 		}
 		
 		
