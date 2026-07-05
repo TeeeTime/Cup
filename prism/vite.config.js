f983ded71994d006fd3eb4dd02875cb64825d275
Tom <eddie@sinistra.de>
2026-07-05 18:56:36 +0200

    Added Startup Banner to Prism

--- prism/vite.config.js
+++ prism/vite.config.js
@@ -3,12 +3,33 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import fs from 'fs'
+
+const startupBannerPlugin = () => ({
+  name: 'startup-banner',
+  configureServer(server) {
+    server.httpServer?.once('listening', () => {
+      try {
+        // Read the file
+        const banner = fs.readFileSync('./banner.txt', 'utf-8');
+
+        // Print it with ANSI color codes (Cyan)
+        console.log(`\n\x1b[36m${banner}\x1b[0m`);
+
+        console.log(`\x1b[33m:: Prism Frontend ::`);
+      } catch (e) {
+
+      }
+    });
+  }
+});
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    startupBannerPlugin()
   ],
   resolve: {
     alias: {
